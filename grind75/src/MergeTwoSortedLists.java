706a102abd9ba12c4f4eeb6e3594a87b805e5105
shreyas <shreyasdevadiga99@gmail.com>
2024-08-11 15:56:07 +0530

    ADDED : MergeTwoSortedLists.java

---
+++ grind75/src/MergeTwoSortedLists.java
@@ -0,0 +1,66 @@
+public class MergeTwoSortedLists {
+
+    public static class ListNode {
+
+        int val;
+        ListNode next;
+
+        ListNode() {}
+
+        ListNode(int val) {
+            this.val = val;
+        }
+
+        ListNode(int val, ListNode next) {
+            this.val = val;
+            this.next = next;
+        }
+    }
+
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+
+        ListNode dummy = new ListNode();
+        ListNode curr = dummy;
+
+        while (list1 != null && list2 != null) {
+            if (list1.val > list2.val) {
+                curr.next = list2;
+                list2 = list2.next;
+            } else {
+                curr.next = list1;
+                list1 = list1.next;
+            }
+            curr = curr.next;
+        }
+        curr.next = (list1 != null) ? list1 : list2;
+
+        return dummy.next;
+    }
+
+    public static void printList(ListNode node) {
+        while (node != null) {
+            System.out.print(node.val + " ");
+            node = node.next;
+        }
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        // Creating the first sorted list: 1 -> 2 -> 4
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(4);
+
+        // Creating the second sorted list: 1 -> 3 -> 4
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
+
+        MergeTwoSortedLists solution = new MergeTwoSortedLists();
+        ListNode mergedList = solution.mergeTwoLists(list1, list2);
+
+        System.out.print("Merged List: ");
+        printList(mergedList);
+    }
+}
